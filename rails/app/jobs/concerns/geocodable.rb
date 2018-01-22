d510340c3b8e8b97630d34e5ebfa9c54594584ee
Hondo <hi@hondo.co>
2018-01-22 15:31:41 -0500

    Change to use mapbox geocoder

--- rails/app/jobs/concerns/geocodable.rb
+++ rails/app/jobs/concerns/geocodable.rb
@@ -5,10 +5,11 @@ module Geocodable
 
   def geocode_address(street_address:, locality: 'Boston', region: 'MA')
     street_address.gsub!(/\s#\d+/i, '')
-    response = Faraday.get('https://search.mapzen.com/v1/search/structured',
-                           { api_key: Rails.application.secrets.mapzen_api_key,
-                             address: street_address, locality: locality, region: region })
-    raise ResponseError, "MapZen Geocoder Error #{response.status}: #{response.body}" unless response.success?
+    address = URI.encode("#{street_address} Boston MA")
+    response = Faraday.get("https://api.mapbox.com/geocoding/v5/mapbox.places/#{address}.json", { access_token: Rails.application.secrets.mapbox_api_key})
+    puts response.body.to_yaml
+    
+    raise ResponseError, "MapBox Geocoder Error #{response.status}: #{response.body}" unless response.success?
     raise NoFeaturesFoundError if JSON.parse(response.body)['features'].blank?
     coordinates = JSON.parse(response.body)['features'][0]['geometry']['coordinates']
     return 'POINT(' + coordinates[0].to_s + ' ' + coordinates[1].to_s + ')'
