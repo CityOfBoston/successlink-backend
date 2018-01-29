6991df5497a09b04258bf801520703277c5fef25
Corey Oordt <coreyoordt@gmail.com>
2018-01-29 14:06:17 -0600

    Updating entrypoint to check for debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,11 @@ if [ -z "$AWS_S3_CONFIG_URL" ]; then
   exit 1
 fi
 
-if [ -ne "$AWS_S3_CONFIG_URL" "DEBUG" ]; then
+if [ "$AWS_S3_CONFIG_URL" != "DEBUG" ]; then
     echo "entrypoint.sh: syncing $AWS_S3_CONFIG_URL to container"
     aws s3 sync $AWS_S3_CONFIG_URL .
+else
+    echo "entrypoint.sh: skipping S3 sync"
 fi
 
 echo "entrypoint.sh: command $@"
